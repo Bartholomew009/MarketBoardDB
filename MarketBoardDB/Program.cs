f9b54f0afb7ac213940b63d4644e298df4c815fe
barholomew009 <reverett93@live.com>
2016-01-22 20:07:37 -0600

    Created Project

---
+++ MarketBoardDB/Program.cs
@@ -0,0 +1,26 @@
+﻿//Ryan Everett
+//Jan. 22, 2016 (c)
+//Market Board DB
+//Idea: Create an interface for a database that tracks an MMO's player market.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MarketBoardDB
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}

f733cbfeeed97686450df7a4f7c772025905e6cd
barholomew009 <reverett93@live.com>
2016-01-22 21:16:09 -0600

    Set the form up. Started connection code.

--- MarketBoardDB/Form1.Designer.cs
+++ MarketBoardDB/Form1.Designer.cs
@@ -28,12 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.lblCost = new System.Windows.Forms.Label();
+            this.lblItem = new System.Windows.Forms.Label();
+            this.cmbItem = new System.Windows.Forms.ComboBox();
+            this.txtBoxCost = new System.Windows.Forms.TextBox();
+            this.cmbBoard = new System.Windows.Forms.ComboBox();
+            this.lblBoard = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(318, 7);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 0;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblCost
+            // 
+            this.lblCost.AutoSize = true;
+            this.lblCost.Location = new System.Drawing.Point(175, 12);
+            this.lblCost.Name = "lblCost";
+            this.lblCost.Size = new System.Drawing.Size(31, 13);
+            this.lblCost.TabIndex = 1;
+            this.lblCost.Text = "Cost:";
+            this.lblCost.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblItem
+            // 
+            this.lblItem.AutoSize = true;
+            this.lblItem.Location = new System.Drawing.Point(12, 12);
+            this.lblItem.Name = "lblItem";
+            this.lblItem.Size = new System.Drawing.Size(30, 13);
+            this.lblItem.TabIndex = 2;
+            this.lblItem.Text = "Item:";
+            this.lblItem.Click += new System.EventHandler(this.lblItem_Click);
+            // 
+            // cmbItem
+            // 
+            this.cmbItem.FormattingEnabled = true;
+            this.cmbItem.Location = new System.Drawing.Point(48, 9);
+            this.cmbItem.Name = "cmbItem";
+            this.cmbItem.Size = new System.Drawing.Size(121, 21);
+            this.cmbItem.TabIndex = 3;
+            // 
+            // txtBoxCost
+            // 
+            this.txtBoxCost.Location = new System.Drawing.Point(212, 9);
+            this.txtBoxCost.Name = "txtBoxCost";
+            this.txtBoxCost.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxCost.TabIndex = 4;
+            // 
+            // cmbBoard
+            // 
+            this.cmbBoard.FormattingEnabled = true;
+            this.cmbBoard.Location = new System.Drawing.Point(48, 36);
+            this.cmbBoard.Name = "cmbBoard";
+            this.cmbBoard.Size = new System.Drawing.Size(121, 21);
+            this.cmbBoard.TabIndex = 5;
+            // 
+            // lblBoard
+            // 
+            this.lblBoard.AutoSize = true;
+            this.lblBoard.Location = new System.Drawing.Point(4, 39);
+            this.lblBoard.Name = "lblBoard";
+            this.lblBoard.Size = new System.Drawing.Size(38, 13);
+            this.lblBoard.TabIndex = 6;
+            this.lblBoard.Text = "Board:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 122);
+            this.Controls.Add(this.lblBoard);
+            this.Controls.Add(this.cmbBoard);
+            this.Controls.Add(this.txtBoxCost);
+            this.Controls.Add(this.cmbItem);
+            this.Controls.Add(this.lblItem);
+            this.Controls.Add(this.lblCost);
+            this.Controls.Add(this.btnInsert);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Label lblCost;
+        private System.Windows.Forms.Label lblItem;
+        private System.Windows.Forms.ComboBox cmbItem;
+        private System.Windows.Forms.TextBox txtBoxCost;
+        private System.Windows.Forms.ComboBox cmbBoard;
+        private System.Windows.Forms.Label lblBoard;
     }
 }
 
--- MarketBoardDB/Form1.cs
+++ MarketBoardDB/Form1.cs
@@ -7,14 +7,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.SQLite;
 
 namespace MarketBoardDB
 {
     public partial class Form1 : Form
     {
+        public static string connect = "Data Source = C:\\Users\\tachikmoa\\Documents\\School\\SQLsp16\\Market.db; Version=3;";
+        SQLiteConnection me = new SQLiteConnection(connect);
+        
         public Form1()
         {
             InitializeComponent();
+            me.Open();
+            string getTables = "SELECT name FROM Market.sqlite_master WHERE type = 'table';";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblItem_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
